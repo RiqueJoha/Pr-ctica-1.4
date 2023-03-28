961dd190778faf5bc29c1735d29c516b8f17fcda
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-03-27 21:29:54 -0300

    primer commit

--- algoritmo.js
+++ algoritmo.js
@@ -1,6 +1,8 @@
-document.querySelector("body").innerHTML=`<details>Título info importante</details>
-<summary>Título importante 2</summary>
+document.querySelector("body").innerHTML=`
+<details>
+<summary>Título de info importante </summary>
 <img height= "200px"src="https://th.bing.com/th/id/R.a7396e3a82bfb6c0236da11e06c85dbb?rik=vI0aEMHzwdfW3Q&pid=ImgRaw&r=0" alt="Gatito">
-<iframe src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>
+<iframe width="600px" height="100px"  src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>
 <p>Información Importante</p>
-<a href="https://www.google.com.ar/">LINk</a>`
+<a href="https://www.google.com.ar/">LINk</a>
+<details>`
